1780885ce91133d8b2055ac4b7b10ec9b53af9dd
Jeremías Meza Suarez <87507111+JeremiasMeza@users.noreply.github.com>
2025-07-28 13:49:01 -0400

    Switch to SQLite

--- Backend/muebleria/settings.py
+++ Backend/muebleria/settings.py
@@ -119,16 +119,11 @@ CORS_ALLOWED_ORIGINS = [
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-from decouple import config
-
+# Use SQLite database stored in project root "bd" directory
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST', default='127.0.0.1'),
-        'PORT': config('DB_PORT', default='3306'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR.parent / 'bd' / 'db.sqlite3',
     }
 }
 
